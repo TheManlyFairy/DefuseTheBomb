48e2c009e9ba144113a7f64ce0813cfc3c1e32c0
Aviv <komemiav@gmail.com>
2018-01-16 10:33:25 +0200

    Created new branch
    This is the shape buttons riddle.
    
    In this riddle, the Manager script shuffles the given textures and then assigns them to the four ShapeButtons in the scene. A dictionary holds each textures' value and this decides the order in which the buttons need to be pressed.
    
    If you want to test the game, run it and look at the ShapeButtonManager in the inspector. The order in which the names of buttons appear (from top to bottom) is the order they need to be pressed to pass the puzzle.

---
+++ Assets/Scripts/ShapeButton.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeButton : MonoBehaviour, IComparable<ShapeButton> {
+
+    ShapeButtonManager sbm;
+    Material ButtonLight;
+    bool isPressed;
+    public int orderID;
+
+	// Use this for initialization
+	void Start ()
+    {
+        ButtonLight = transform.GetChild(0).GetComponent<Renderer>().material;
+        isPressed = false;
+        sbm = ShapeButtonManager.sbManager;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100))
+                if (!hit.collider.gameObject.GetComponent<ShapeButton>().isPressed)
+                {
+                    ShapeButtonManager.CheckPressOrder(hit.collider.gameObject.GetComponent<ShapeButton>().orderID);
+                }
+        }
+	}
+
+    public void LightUp()
+    {
+        isPressed = true;
+        ButtonLight.SetColor("_EmissionColor", Color.green);
+    }
+
+    public void LightOff()
+    {
+        isPressed = false;
+        ButtonLight.SetColor("_EmissionColor", new Color(0.5f, 0.5f, 0.5f));
+    }
+    public void SetButton(int id, Texture tex)
+    {
+        orderID = id;
+        GetComponent<Renderer>().material.SetTexture("_MainTex", tex);
+    }
+    public int CompareTo(ShapeButton other)
+    {
+        throw new NotImplementedException();
+    }
+
+    
+}
+
+public class SortByOrderId : IComparer<ShapeButton>
+{
+    public int Compare(ShapeButton a, ShapeButton b)
+    {
+        return a.orderID - b.orderID;
+    }
+}
---
+++ Assets/Scripts/ShapeButtonManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeButtonManager : MonoBehaviour {
+
+    public static ShapeButtonManager sbManager;
+    public List<Texture> textures;
+    public List<ShapeButton> buttons;
+    static List<ShapeButton> staticButtons;
+    Dictionary<Texture, int> dict;
+    static int nextButton;
+
+    void Awake () 
+    {
+        if (sbManager == null)
+            sbManager = this;
+        else
+            Destroy(this.gameObject);
+
+        InitDictionary(); // for storing each textures and its order ID
+        ShuffleTextures(); // shuffles the texture list
+        InitButtons(); // initiate four buttons with random textures
+        staticButtons = buttons;
+        nextButton = 0;
+    }
+
+    void InitButtons()
+    {
+        //buttons = new ShapeButton[4];
+        for (int i=0; i< buttons.Count; i++)
+        {
+            buttons[i].SetButton(dict[textures[i]], textures[i]);
+        }
+
+        buttons.Sort(new SortByOrderId());
+    }
+    void InitDictionary()
+    {
+        dict = new Dictionary<Texture, int>();
+        for (int i = 0; i < textures.Count; i++)
+        {
+            dict.Add(textures[i], i);
+        }
+    }
+    void ShuffleTextures()
+    {
+        int length = textures.Count;
+        for (int i = 0; i < length; i++)
+        {
+            int random = i + Random.Range(0, length - i);
+            Texture t = textures[random];
+            textures[random] = textures[i];
+            textures[i] = t;
+        }
+    }
+
+    public static void CheckPressOrder(int buttonId)
+    {
+       if (buttonId == staticButtons[nextButton].orderID)
+        {
+            
+            staticButtons[nextButton].LightUp();
+            nextButton++;
+        }
+        else
+        {
+            ResetAllButtons();
+            nextButton = 0;
+        }
+    }
+    public static void ResetAllButtons()
+    {
+       for (int i=0; i<staticButtons.Count; i++)
+        {
+            staticButtons[i].LightOff();
+        }
+        nextButton = 0;
+    }
+}
