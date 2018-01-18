1c55e4ccc24aa7f898c4e9d31304df9f6714da02
Aviv <komemiav@gmail.com>
2018-01-18 15:48:20 +0200

    Created first part of the UI
    Created a Time Manager to control the countdown timer.
    
    TimeManager script has been added to the UI element currently dubbed "text" and has been set to not destroy on load. This affects the entire hierarchy, meaning the Canvas and Main Camera also remain between scenes.

---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour {
+
+    public static TimeManager timeManager;
+    public int minutes, seconds;
+    float countdownDelay;
+
+    Text text;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (timeManager == null)
+            timeManager = this;
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+
+        countdownDelay = 1f;
+
+        text = GetComponent<Text>();
+
+        StartCoroutine(Countdown());
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(1);
+        }
+	}
+
+    IEnumerator Countdown()
+    {
+        while (!(minutes == 0 && seconds == 0))
+        {
+            yield return new WaitForSeconds(countdownDelay);
+            seconds--;
+            if (seconds < 0)
+            {
+                seconds = 59;
+                minutes--;
+            }
+
+            text.text = DigitalTimeDisplay();
+        }
+    }
+
+    public string DigitalTimeDisplay()
+    {
+        string secondString, minuteString;
+        if (seconds < 10)
+            secondString = "0" + seconds;
+        else
+            secondString = "" + seconds;
+
+        if (minutes < 10)
+            minuteString = "0" + minutes;
+        else
+            minuteString = "" + minutes;
+
+        return minuteString + ":" + secondString;
+    }
+}
