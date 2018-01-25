1c9d87da4da36dab0384c1f107cacf8e0324e459
Lionheart <hallel1851998@gmail.com>
2018-01-25 13:37:49 +0200

    MazePuzzle
    MazePuzzle added to branch
    
    bug fixes:
    - proper detection of falling off the board
    - player now freezes for 2 seconds after failing to disable endless fail

---
+++ Assets/Scripts/FileManager.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+using UnityEditor;
+
+public class FileManager : MonoBehaviour {
+
+    public static FileManager instance;
+
+    private void Awake()
+    {
+        Debug.Log(Application.dataPath);
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    public void Create(string filename)
+    {
+        try
+        {
+            if (File.Exists(Application.persistentDataPath + "/" + filename))
+            {
+                throw new Exception("file already exists");
+            }
+            else
+            {
+                FileStream file = File.Create(Application.persistentDataPath + "/" + filename);
+                file.Close();
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+    }
+
+    public void Save<T>(T data,string filename)
+    {
+        try
+        {
+            if (!File.Exists(Application.persistentDataPath + "/" + filename))
+                Create(filename);
+            string json = JsonUtility.ToJson(data);
+            File.WriteAllText(Application.persistentDataPath + "/" + filename,json);
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+    }
+
+    public T Load<T>(TextAsset txt)
+    {
+        try
+        {
+            string json = txt.text;
+            T res = JsonUtility.FromJson<T>(json);
+            return res;
+        }
+        catch(Exception e)
+        {
+            Debug.Log(e.Message);
+            return default(T);
+        }
+    }
+
+    public T Load<T>(string filename)
+    {
+        try
+        {
+            if (File.Exists(Application.persistentDataPath + "/" + filename))
+            {
+                string json = File.ReadAllText(Application.persistentDataPath + "/" + filename);
+                T res = JsonUtility.FromJson<T>(json);
+                return res;
+            }
+            else
+                return default(T);
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+            return default(T);
+        }
+    }
+
+
+}
